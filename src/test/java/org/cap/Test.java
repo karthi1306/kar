607f2795816ff8135762aa193878a22e0339042d
karthi1306 <skarthick0113@gmail.com>
2021-01-18 13:27:11 +0530

    checking

--- src/test/java/org/cap/Test.java
+++ src/test/java/org/cap/Test.java
@@ -2,7 +2,7 @@ package org.cap;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Welcome to new framework");
+		System.out.println("new framework");
 		System.out.println("add to my fav");
 	}
 
